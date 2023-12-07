9075eb428492371906debb93baabf43ea141e94a
Vitória Knupp <arq.vitoriaknupp@gmail.com>
2023-12-07 19:40:11 -0300

    add: header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,17 +1,22 @@
 import React from 'react';
 import styles from './Header.module.css';
 import { Link } from 'react-router-dom';
+import Logo from '../Assets/Logo.svg';
 
 const Header = () => {
   return (
-    <div>
-      <nav className="container">
-        <Link to="/">home</Link>
-        <Link to="/about">about</Link>
-        <Link to="/work">work</Link>
-        <Link to="/contact">contact</Link>
+    <header className={styles.header}>
+      <nav className={`${styles.nav} container`}>
+        <div className={styles.menu}>
+          <Link to="/about">about</Link>
+          <Link to="/work">work</Link>
+          <Link to="/contact">contact</Link>
+        </div>
+        <Link to="/">
+          <img src={Logo} alt="logo" />
+        </Link>
       </nav>
-    </div>
+    </header>
   );
 };
 
